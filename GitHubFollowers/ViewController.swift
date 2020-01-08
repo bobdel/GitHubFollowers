0ae7f615de503dde56c78ed2b1505b39657aa7b8
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-01-08 10:00:00 -0800

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Robert DeLaurentis on 1/8/20.
+//  Copyright © 2020 Robert DeLaurentis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
