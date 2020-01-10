40de031a56f8980d7a125af2dfeb8305d9a5e901
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-01-10 07:52:32 -0800

    Added custom button and text field objects. first video in second section.

---
+++ GitHubFollowers/UI Components/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Robert DeLaurentis on 1/10/20.
+//  Copyright © 2020 Robert DeLaurentis. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // init required by the API to support storyboards
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // configure object specific visual properties of button
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero) // size created by autolayout
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    // configure common visual properties of button
+    func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
