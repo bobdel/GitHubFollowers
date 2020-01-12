7ad7d4b4d1f319b96b86479a1862a08c2d158d18
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-01-12 14:29:05 -0800

    Refactored to use built-in Result type. This completes section 3 networking.

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -23,11 +23,11 @@ class NetworkManager {
     ///   - username: A valid GitHub username string
     ///   - page: a integer for the page number
     ///   - completed: a closure for the network request and result handler
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil, .invalidUsername)
+            completed(.failure(.invalidUsername))
             return
         }
         
@@ -35,19 +35,19 @@ class NetworkManager {
             
             // handle the error. If error not nil call completion handler with error message
             if let _ = error {
-                completed(nil, .unableToComplete)
+                completed(.failure(.unableToComplete))
                 return
             }
             
             // handle the response. Check for success status code or call completion handler
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, .invalidResponse)
+                completed(.failure(.invalidResponse))
                 return
             }
             
             // handle the data
             guard let data = data else {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
                 return
             }
             
@@ -55,9 +55,9 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                completed(.success(followers))
             } catch {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
             }
         }
         
--- GitHubFollowers/Utilities/GFError.swift
+++ GitHubFollowers/Utilities/GFError.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 /// Error Messages with raw values for user display
-enum ErrorMessage: String {
+enum GFError: String, Error {
     case invalidUsername = "This username created an invalid request. Please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
