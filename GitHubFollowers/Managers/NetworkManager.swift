0a8cfc336ac17b829105d8eec93e77c78279b455
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-01-12 13:38:14 -0800

    Refactored error message to use an enum with associated type strings. This method is common in code bases that do not have a result type. Next commit will add the Swift result type.

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -23,11 +23,11 @@ class NetworkManager {
     ///   - username: A valid GitHub username string
     ///   - page: a integer for the page number
     ///   - completed: a closure for the network request and result handler
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil, "This username created an invalid request. Please try again")
+            completed(nil, .invalidUsername)
             return
         }
         
@@ -35,19 +35,19 @@ class NetworkManager {
             
             // handle the error. If error not nil call completion handler with error message
             if let _ = error {
-                completed(nil, "Unable to complete your request. Please check your internet connection.")
+                completed(nil, .unableToComplete)
                 return
             }
             
             // handle the response. Check for success status code or call completion handler
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "Invalid response from the server. Please try again.")
+                completed(nil, .invalidResponse)
                 return
             }
             
             // handle the data
             guard let data = data else {
-                completed(nil, "The data received from the server was invalid. Please try again.")
+                completed(nil, .invalidData)
                 return
             }
             
@@ -57,7 +57,7 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(followers, nil)
             } catch {
-                completed(nil, "Server response invalid. Please try again")
+                completed(nil, .invalidData)
             }
         }
         
---
+++ GitHubFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorMessage.swift
+//  GitHubFollowers
+//
+//  Created by Robert DeLaurentis on 1/12/20.
+//  Copyright © 2020 Robert DeLaurentis. All rights reserved.
+//
+
+import Foundation
+
+/// Error Messages with raw values for user display
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again." // this is used two places. Should refactor so its only used once, or add error logging to keep track of call site.
+}
