75f5cb44f583ee080416db9e77f2cd24a0e1c477
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-01-12 17:43:33 -0800

    Added flow layout. CollectionView video 3 complete. Note flow layout can be refactored into a helper file to shorten VC.

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -38,10 +38,24 @@ class FollowerListViewController: UIViewController {
     }
 
     private func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width // screen width
+        let padding: CGFloat = 12 // UIEdgeInsets
+        let minimumItemSpacing: CGFloat = 10 // column width
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2) // space available for cell content
+        let itemWidth = availableWidth / 3 // column width
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
     
     private func getFollowers() {
