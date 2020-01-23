3e92c1c622943c156caaf6c0efd1791b5a95ae25
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-01-23 15:15:22 -0800

    Added GFUserInfoHeaderViewController. Video 4 complete. (no code in video 3)

---
+++ GitHubFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by Robert DeLaurentis on 1/23/20.
+//  Copyright © 2020 Robert DeLaurentis. All rights reserved.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    /// init required by the API to support storyboards
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(fontSize: CGFloat) {
+        self.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Robert DeLaurentis on 1/23/20.
+//  Copyright © 2020 Robert DeLaurentis. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
