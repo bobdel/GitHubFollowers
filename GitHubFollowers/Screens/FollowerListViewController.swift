f3e126fd7adb4dece8329efc8236bfbe1d38a7da
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-01-25 17:12:19 -0800

    Added Add button to FollowersListVC. Completed Video 1 of section Favorites Screen Persistence.

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -52,6 +52,9 @@ class FollowerListViewController: UIViewController {
     private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
 
     private func configureCollectionView() {
@@ -112,6 +115,10 @@ class FollowerListViewController: UIViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
+    }
+    
+    @objc func addButtonTapped() {
+        print("tapped")
     }
     
 }
