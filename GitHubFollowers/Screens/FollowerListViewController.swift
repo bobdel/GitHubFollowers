0dc41e682069f40f32024060c27ac702e93073c2
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-02-02 15:57:56 -0800

    Bugfix on Search Results

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -80,7 +80,6 @@ class FollowerListViewController: GFDataLoadingViewController {
     private func configureSearchController() {
         let searchController = UISearchController()
         searchController.searchResultsUpdater = self
-        searchController.searchBar.delegate = self
         searchController.searchBar.placeholder = "Search for a username"
         searchController.obscuresBackgroundDuringPresentation = false
         navigationItem.searchController = searchController
@@ -199,21 +198,21 @@ extension FollowerListViewController: UICollectionViewDelegate {
     }
 }
 
-/// UISearchController Delegate Conformance
-extension FollowerListViewController: UISearchResultsUpdating, UISearchBarDelegate {
+/// UISearchController Search Results
+extension FollowerListViewController: UISearchResultsUpdating {
 
     func updateSearchResults(for searchController: UISearchController) {
-        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            filteredFollowers.removeAll()
+            updateData(on: followers)
+            isSearching = false
+            return
+        }
 
         isSearching = true
         filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filteredFollowers)
 
-    }
-
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        isSearching = false
-        updateData(on: followers)
     }
 }
 
